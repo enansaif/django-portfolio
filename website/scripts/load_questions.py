b66afbab37d8f9672359e11c118832c9117ac478
rect1fy <enansaifme33@gmail.com>
2023-06-19 21:57:00 +0600

    added csv processing script

---
+++ website/scripts/load_questions.py
@@ -0,0 +1,24 @@
+import csv
+from leetquizzer.models import Problem, Topic, Difficulty
+
+def run():
+    file = open("leetquizzer/questions/problems.csv")
+    reader = csv.reader(file)
+    next(reader)
+    
+    count = 0
+    for row in reader:
+        number, name = row[0], row[1]
+        hasNumber = Problem.objects.filter(number=number).exists()
+        hasName = Problem.objects.filter(name=name).exists()
+        if hasNumber or hasName:
+            continue
+        topic, _ = Topic.objects.get_or_create(name=row[2])
+        difficulty, _ = Difficulty.objects.get_or_create(name=row[3])
+        
+        problem = Problem(number=number, name=name, topic=topic, difficulty=difficulty, 
+                          solution=row[4], option1=row[5], option2=row[6], edge_case=row[7])
+        problem.save()
+        count += 1
+    
+    print(f"{count} Problems added to Database")
