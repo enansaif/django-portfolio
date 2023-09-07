a177896907d1a809287c7d3878264f6c5c6e18b8
rect1fy <enansaifme33@gmail.com>
2023-09-07 17:13:03 +0600

    added back login success url, added signup page

--- authenticator/views.py
+++ authenticator/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.views import View
+from django.contrib.auth.forms import UserCreationForm
 
 
 def logout_view(request):
@@ -27,6 +28,7 @@ class LoginUser(View):
     If the authentication is successful, logs in the user and redirects to the request URL.
     Otherwise, displays an error message and redirects back to the login page.
     """
+    success_url = reverse_lazy('portfolio:base')
 
     def get(self, request):
         """
@@ -43,6 +45,21 @@ class LoginUser(View):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect(request.GET.get('next', self.request.path_info))
+            return redirect(request.GET.get('next', self.success_url))
         messages.error(request, "Invalid username or password")
+        return redirect(self.request.path_info)
+
+
+class Signup(View):
+    success_url = reverse_lazy('portfolio:base')
+
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "auth/signup.html", {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(request.GET.get('next', self.success_url))
         return redirect(self.request.path_info)
