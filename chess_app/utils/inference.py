1387a08f6af8bdac804d0e28816b0adb9d34ad89
enansaif <enansaifme33@gmail.com>
2024-02-04 20:49:30 +0600

    fixed ai promotion but

--- chess_app/utils/inference.py
+++ chess_app/utils/inference.py
@@ -2,6 +2,7 @@ import numpy as np
 from onnxruntime import InferenceSession
 from .functions import board_repr, move_gen
 from django.contrib.staticfiles import finders
+from .functions import get_game_state
 
 def predict(board):
     for move in board.legal_moves:
@@ -15,5 +16,10 @@ def predict(board):
     outs = session.run(None, inputs)
     pred = np.squeeze(outs[0])
     move = move_gen(pred, board)
+    
+    game_state = get_game_state(board)
+    if move in game_state["promotions"]:
+        move += 'q'
+    
     board.push_uci(move)
     return board.pop()
